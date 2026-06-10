5f26240704b1c47d87b3ec099f0303819823a2f8
jeanchristophe-Git <hello.jeanchristophebogbe@gmail.com>
2026-06-10 08:11:58 +0000

    fix(back-office): supprime subtitle invalide sur TopNav journal

--- back-office/app/(admin)/journal/page.tsx
+++ back-office/app/(admin)/journal/page.tsx
@@ -4,7 +4,7 @@ import { JournalTable } from './JournalTable'
 export default function JournalPage() {
   return (
     <>
-      <TopNav title="Journal d'activité" subtitle="Traçabilité complète des actions sur la plateforme" />
+      <TopNav title="Journal d'activité" />
       <div className="p-6 flex flex-col gap-4">
         <JournalTable />
       </div>
